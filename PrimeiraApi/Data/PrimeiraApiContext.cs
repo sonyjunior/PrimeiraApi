e606d238a8efa7411c456331332708e351db4050
sonyjunior <antonio.rolim2@etec.sp.gov.br>
2021-04-16 17:18:43 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraApi/Data/PrimeiraApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrimeiraApi.Models;
+
+namespace PrimeiraApi.Data
+{
+    public class PrimeiraApiContext : DbContext
+    {
+        public PrimeiraApiContext (DbContextOptions<PrimeiraApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PrimeiraApi.Models.Cliente> Cliente { get; set; }
+    }
+}
